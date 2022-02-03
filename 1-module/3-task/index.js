cbacaf413031a1971f3bc8f088db56d0c299d0a5
Alex Boykova <alexaboyka@gmail.com>
2022-02-04 00:03:05 +0300

    create func firstLetterToUpperCase

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,12 @@
 function ucFirst(str) {
   // ваш код...
+  if (!!str) {
+    firstLetter = str[0].toUpperCase();
+    EndOfTheWord = str.substring(1, str.length);
+    str = firstLetter + EndOfTheWord;
+  }
+  return str;
 }
+
+
+
