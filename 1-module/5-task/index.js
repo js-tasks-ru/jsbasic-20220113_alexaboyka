771915744858d9cb0c5a3bc3b310ea473401b5e2
Alex Boykova <alexaboyka@gmail.com>
2022-02-09 23:02:12 +0300

    resolve 5-task some message

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,11 @@
 function truncate(str, maxlength) {
   // ваш код...
+  if (str.length > maxlength) {
+    str = str.slice(0, maxlength - 1) + '…';
+  }
+  return str;
 }
+/* let str = 'Вот, что мне хотелось бы сказать на эту тему:';
+let maxlength = 20;
+console.log(truncate(str, maxlength));
+ */
