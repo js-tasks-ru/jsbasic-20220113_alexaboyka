b4a7c120ba668d064e80b655feea9bcfae2b5a0c
Alex Boykova <alexaboyka@gmail.com>
2022-02-18 21:49:37 +0300

    complete 3-module

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -16,7 +16,3 @@ function getMinMax(str) {
   result.max = numArr[numArr.length - 1];
   return result;
 }
-
-
-let inputData = '1 и -5.8 или 10 хотя 34 + -5.3 и 73';
-console.log(getMinMax(inputData));
